49a8ac41ac7e58bf4c33f602bec01d0582b09588
jaqueline-amanda <jakamanda011@gmail.com>
2024-03-19 09:16:57 -0300

    criando projeto com spring

---
+++ src/test/java/br/com/curso/rentacar/RentAcarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
